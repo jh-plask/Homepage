903676c9e62446bfe9ebc9eee8fa3028ec2c4e65
jh-plask <junho@plask.ai>
2023-02-21 20:34:12 +0900

    first

---
+++ src/pages/index.jsx
@@ -0,0 +1,40 @@
+import Head from 'next/head'
+
+import { CallToAction } from '@/components/CallToAction'
+import { Faqs } from '@/components/Faqs'
+import { Footer } from '@/components/Footer'
+import Header from '@/components/Header'
+import { Hero } from '@/components/Hero'
+import { Pricing } from '@/components/Pricing'
+import { PrimaryFeatures } from '@/components/PrimaryFeatures'
+import { SecondaryFeatures } from '@/components/SecondaryFeatures'
+import { Testimonials } from '@/components/Testimonials'
+import Banner from '@/components/Banner'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Plask - 3D Animation made simple by AI</title>
+        <meta
+          name="description"
+          content="Most bookkeeping software is accurate, but hard to use. We make the opposite trade-off, and hope you don’t get audited."
+        />
+      </Head>
+      <Header />
+      <main>
+        <div >
+        <Hero />
+        <PrimaryFeatures />
+        <SecondaryFeatures />
+        <CallToAction />
+        <Testimonials />
+        <Pricing />
+        <Faqs />
+        </div>
+      </main>
+      <Footer />
+      
+    </>
+  )
+}
